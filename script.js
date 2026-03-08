c37b5315e6f92294b07d65450c927e9be5c23f02
jobinpjosephjobin-lang <jobinpjosephjobin@gmail.com>
2026-03-08 06:54:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,85 +1,90 @@
-const API_KEY = "724bac074007bc29092fc265866757b1";
+const map = L.map('map').setView([20,0],3);
 
-const map = L.map('map').setView([20,0], 3);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+L.tileLayer(
+'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+{
 maxZoom:19
-}).addTo(map);
+}
+).addTo(map);
 
-let planeMarkers = [];
+let aircraftMarkers = [];
 
-
-/* plane icon */
-function createPlaneIcon(rotation){
+function createPlane(rotation){
 
 return L.divIcon({
+
 className:"",
-html:`<img class="plane-icon" 
-style="transform:rotate(${rotation}deg)" 
-src="https://cdn-icons-png.flaticon.com/512/34/34627.png">`,
-iconSize:[30,30]
+
+html:`<img 
+class="plane"
+src="https://cdn-icons-png.flaticon.com/512/34/34627.png"
+style="transform:rotate(${rotation}deg)">`,
+
+iconSize:[26,26]
+
 });
+
 }
-
-
-/* get flights */
 
 async function loadFlights(){
 
-const url = `http://api.aviationstack.com/v1/flights?access_key=${API_KEY}`;
+const response = await fetch(
+"https://opensky-network.org/api/states/all"
+);
 
-const res = await fetch(url);
-const data = await res.json();
+const data = await response.json();
 
-planeMarkers.forEach(m=>map.removeLayer(m));
-planeMarkers=[];
+aircraftMarkers.forEach(m=>map.removeLayer(m));
+aircraftMarkers=[];
 
-data.data.forEach(flight=>{
+data.states.forEach(flight=>{
 
-if(!flight.live) return;
+const lon = flight[5];
+const lat = flight[6];
+const altitude = flight[7];
+const velocity = flight[9];
+const direction = flight[10];
+const callsign = flight[1];
+const country = flight[2];
 
-let lat = flight.live.latitude;
-let lon = flight.live.longitude;
-let dir = flight.live.direction || 0;
+if(!lat || !lon) return;
 
 let marker = L.marker(
 [lat,lon],
-{icon:createPlaneIcon(dir)}
+{icon:createPlane(direction || 0)}
 ).addTo(map);
 
-marker.bindPopup(`
-<div class="popup">
-<b>Flight:</b> ${flight.flight.iata || "N/A"}<br>
-<b>Airline:</b> ${flight.airline.name}<br>
-<b>From:</b> ${flight.departure.airport}<br>
-<b>To:</b> ${flight.arrival.airport}<br>
-<b>Altitude:</b> ${flight.live.altitude} ft<br>
-<b>Speed:</b> ${flight.live.speed_horizontal} km/h
-</div>
-`);
+marker.bindPopup(
 
-planeMarkers.push(marker);
+`
+<b>Flight:</b> ${callsign || "Unknown"}<br>
+<b>Country:</b> ${country}<br>
+<b>Altitude:</b> ${altitude || "N/A"} m<br>
+<b>Speed:</b> ${velocity || "N/A"} m/s
+`
+
+);
+
+aircraftMarkers.push(marker);
 
 });
 
 }
 
-/* user location */
-
-function locateUser(){
+function getUserLocation(){
 
 if(navigator.geolocation){
 
 navigator.geolocation.getCurrentPosition(pos=>{
 
-let lat = pos.coords.latitude;
-let lon = pos.coords.longitude;
+const lat = pos.coords.latitude;
+const lon = pos.coords.longitude;
 
 map.setView([lat,lon],6);
 
 L.marker([lat,lon])
 .addTo(map)
-.bindPopup("Your Location")
+.bindPopup("📍 Your Location")
 .openPopup();
 
 });
@@ -88,9 +93,7 @@ L.marker([lat,lon])
 
 }
 
-/* refresh every 15 sec */
+loadFlights();
+getUserLocation();
 
 setInterval(loadFlights,15000);
-
-locateUser();
-loadFlights();
